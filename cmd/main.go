a349cedd957293889398f3835f0ec302f68e5f75
Tazhben <tazhben.arsu@gmail.com>
2022-02-18 14:33:29 +0600

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Fatalln(fmt.Sprintf("Service shut down: %v", run()))
+}
+
+func run() error {
+	//ctx, cancel := context.WithCancel(context.Background())
+	//
+	//defer cancel()
+	//
+	//errCh := make(chan error, 1)
+
+}
---
+++ internal/transport/http/server.go
@@ -0,0 +1,4 @@
+package http
+
+func StartServer(ctx, errCh chan error, cfg config.) {
+}
