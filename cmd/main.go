64584b609757ab5b67a2e5da2fac7b0e0d22eff9
Tazhben <tazhben.arsu@gmail.com>
2022-02-19 13:02:37 +0600

    start server

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"Tazhben/auth/config"
+	"Tazhben/auth/internal/transport/http"
+	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -10,10 +16,23 @@ func main() {
 }
 
 func run() error {
-	//ctx, cancel := context.WithCancel(context.Background())
-	//
-	//defer cancel()
-	//
-	//errCh := make(chan error, 1)
-	return nil
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	errCh := make(chan error, 1)
+
+	configs := config.NewConfigServer()
+
+	go http.StartServer(ctx, errCh, configs)
+
+	go shutDown(errCh)
+
+	return <-errCh
+}
+
+func shutDown(errCh chan<- error) {
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+	errCh <- fmt.Errorf("%s", <-sigCh)
 }
--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -1,4 +1,29 @@
 package http
 
-func StartServer(ctx, errCh chan error, cfg config.) {
+import (
+	"Tazhben/auth/config"
+	"context"
+	"errors"
+	"github.com/labstack/echo"
+	"os"
+)
+
+func StartServer(ctx context.Context, errCh chan error, cfg *config.Config) {
+	app := echo.New()
+
+	pathPrefix := os.Getenv("CONFIG_PATH")
+
+	if len(pathPrefix) == 0 {
+		errCh <- errors.New("CONFIG_PATH is empty")
+	}
+
+	errCh <- app.Start(":8090")
+}
+
+func initConfig() error {
+	pathPrefix := os.Getenv("CONFIG_PATH")
+
+	if len(pathPrefix) == 0 {
+		return errors.New("CONFIG_PATH is empty")
+	}
 }
